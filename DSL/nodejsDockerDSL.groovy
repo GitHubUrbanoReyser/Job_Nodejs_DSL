cb225213b791621bb5a12349e510dcdcec08a408
GitHubUrbanoReyser <106035896+GitHubUrbanoReyser@users.noreply.github.com>
2022-07-22 20:12:35 -0500

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js Docker DSL') {
-    description('Aplicación Node JS Docker DSL para el curso de Jenkins')
+    description('Aplicación Node JS Docker DSL')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/GitHubUrbanoReyser/Job_Nodejs_DSL.git', 'main') { node ->
+            node / gitConfigName('demo')
+            node / gitConfigEmail('r.urbano@gmail.com')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('macloujulian/nodejsapp')
+            repositoryName('GitHubUrbanoReyser/Job_Nodejs_DSL')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
